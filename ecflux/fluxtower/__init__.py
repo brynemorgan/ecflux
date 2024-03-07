5fa06e1e20ceff38d400b79932642283f64e8867
brynemorgan <brynmorgan@ucsb.edu>
2024-03-06 18:25:55 -0800

    Refactoring

--- ecflux/fluxtower/__init__.py
+++
@@ -1,9 +0,0 @@
-from .fluxtower import FluxTower
-from .amf_tower import AmeriFluxTower
-from .ep_tower import EddyProTower
-from .csi_tower import CSITower
-
-from .footprint import Footprint
-
-from .FFP_Python import calc_footprint_FFP as calc_ffp
-from .FFP_Python import calc_footprint_FFP_climatology as calc_ffp_clim
